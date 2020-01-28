2e0d3354b26c15eca04974c03880e90053c2d14a
DeMarcus Kirby <tkd024@gmail.com>
2020-01-28 10:07:04 -0700

    Add feature test for shelter show page

---
+++ spec/features/shelter/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor of a shelter show page' do
+  before(:each) do
+    @shelter1 = Shelter.create!(name: "Joe's Shelter", address: "123 Main St.", city: "Dallas", state: "TX", zip: "75341")
+    @shelter2 = Shelter.create!(name: "Jane's Shelter", address: "321 2nd St.", city: "Denver", state: "CO", zip: "80020")
+  end
+
+  it 'should show the individual shelter show page' do
+    visit "/shelters/#{@shelter1.id}"
+
+    expect(page).to have_content(@shelter1.name)
+    expect(page).to have_content("Address: #{@shelter1.address} #{@shelter1.city}, #{@shelter1.state} #{@shelter1.zip}")
+
+    expect(page).to_not have_content(@shelter2.name)
+    expect(page).to_not have_content("Address: #{@shelter2.address} #{@shelter2.city}, #{@shelter2.state} #{@shelter2.zip}")
+  end
+end
