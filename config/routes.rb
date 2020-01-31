5473c66b387c2b5ad44b4fceec9aaa077321a4c9
DeMarcus Kirby <tkd024@gmail.com>
2020-01-31 11:22:10 -0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: "shelters#edit"
   patch '/shelters/:id', to: "shelters#update"
   delete 'shelters/:id', to: "shelters#destroy"
+
   get '/shelters/:shelter_id/pets', to: "shelterpets#index"
+  get '/shelters/:shelter_id/pets/new', to: "shelterpets#new"
+  post 'shelters/:shelter_id/pets', to: "shelterpets#create"
 
   get '/pets', to: "pets#index"
   get '/pets/:id', to: "pets#show"
